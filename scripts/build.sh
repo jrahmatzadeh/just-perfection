0a83ee65616c1a9d2a69abb4d06288f3a2e6447f
Just Perfection <justperfection.channel@gmail.com>
2021-05-12 16:43:00 -0700

    update copyright

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@
 # Script to build the extension zip and install the package
 #
 # This Script is released under GPL v3 license
-# Copyright (C) 2020 Just Perfection
+# Copyright (C) 2020 Javad Rahmatzadeh
 
 set -e
 
