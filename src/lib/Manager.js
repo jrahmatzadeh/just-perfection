4f14e7eb1e544e0afc67fbf64ec54a31a19f9b84
Javad Rahmatzadeh <j.rahmatzadeh@gmail.com>
2023-04-08 16:41:36 -0700

    typo

--- src/lib/Manager.js
+++ src/lib/Manager.js
@@ -632,25 +632,25 @@ var Manager = class
     {
         let className = 'just-perfection';
         let fallbackClassName = 'just-perfection-gnome3';
-        let fourtySecondGenClassName = 'just-perfection-gnome4x-2nd-gen';
+        let fortySecondGenClassName = 'just-perfection-gnome4x-2nd-gen';
 
         if (forceOriginal || !this._settings.get_boolean('theme')) {
             this._api.UIStyleClassRemove(className);
             this._api.UIStyleClassRemove(fallbackClassName);
-            this._api.UIStyleClassRemove(fourtySecondGenClassName);
+            this._api.UIStyleClassRemove(fortySecondGenClassName);
         } else {
             this._api.UIStyleClassAdd(className);
             if (this._shellVersion < 40) {
                 this._api.UIStyleClassAdd(fallbackClassName);
             }
             if (this._shellVersion >= 42) {
-                this._api.UIStyleClassAdd(fourtySecondGenClassName);
+                this._api.UIStyleClassAdd(fortySecondGenClassName);
             }
         }
     }
 
     /**
-     * apply activites button settings
+     * apply activities button settings
      *
      * @param {boolean} forceOriginal force original shell setting
      *
