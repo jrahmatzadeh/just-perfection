fc8cec6b9a30b5ed8bb6e0d19eafe1be01ee4988
Javad Rahmatzadeh <j.rahmatzadeh@gmail.com>
2025-01-28 04:17:26 -0800

    Optimize Preferences Window for QR Code Visibility
    This commit modifies the preferences window's size and
    the accompanying support text to enhance
    the visibility of the cryptocurrency donation QR code.

--- src/lib/Prefs/Prefs.js
+++ src/lib/Prefs/Prefs.js
@@ -214,8 +214,8 @@ export class Prefs
     {
         let [pmWidth, pmHeight, pmScale] = this.#getPrimaryMonitorInfo();
         let sizeTolerance = 50;
-        let width = 670;
-        let height = 840;
+        let width = 640;
+        let height = 810;
 
         if (
             (pmWidth / pmScale) - sizeTolerance >= width &&
